c523e05e423e75e675e16a3b7044620a12fb49cb
Jamie Quigley <jamie@quigley.xyz>
2022-10-12 20:52:51 +0100

    rnnoise-plugin: 0.91 -> 1.03
    Also add myself as a maintainer

--- pkgs/development/libraries/rnnoise-plugin/default.nix
+++ pkgs/development/libraries/rnnoise-plugin/default.nix
@@ -1,25 +1,47 @@
-{ lib, stdenv, SDL2, fetchFromGitHub, cmake }:
-
+{
+  lib,
+  stdenv,
+  alsa-lib,
+  cmake,
+  fetchFromGitHub,
+  freetype,
+  gtk3-x11,
+  mount,
+  pcre,
+  pkg-config,
+  webkitgtk,
+  xorg,
+}:
 stdenv.mkDerivation rec {
   pname = "rnnoise-plugin";
-  version = "0.91";
+  version = "1.03";
 
   src = fetchFromGitHub {
     owner = "werman";
     repo = "noise-suppression-for-voice";
     rev = "v${version}";
-    sha256 = "11pwisbcks7g0mdgcrrv49v3ci1l6m26bbb7f67xz4pr1hai5dwc";
+    sha256 = "sha256-1DgrpGYF7G5Zr9vbgtKm/Yv0HSdI7LrFYPSGKYNnNDQ=";
   };
 
-  nativeBuildInputs = [ cmake ];
+  nativeBuildInputs = [cmake pkg-config];
 
-  cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" ];
+  buildInputs = [
+    alsa-lib
+    freetype
+    gtk3-x11
+    pcre
+    webkitgtk
+    xorg.libX11
+    xorg.libXrandr
+  ];
+
+  cmakeFlags = ["-DCMAKE_BUILD_TYPE=Release"];
 
   meta = with lib; {
     description = "A real-time noise suppression plugin for voice based on Xiph's RNNoise";
     homepage = "https://github.com/werman/noise-suppression-for-voice";
     license = licenses.gpl3;
     platforms = platforms.all;
-    maintainers = with maintainers; [ panaeon henrikolsson ];
+    maintainers = with maintainers; [panaeon henrikolsson sciencentistguy];
   };
 }
